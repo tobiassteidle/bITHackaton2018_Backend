d2a062b6057a52bb02ebc2af95cb34f9a56fb1e9
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-06-09 11:41:32 +0200

    Update

---
+++ src/main/java/de/bithackaton/ai/NavigationService.java
@@ -0,0 +1,9 @@
+package de.bithackaton.ai;
+
+/**
+ * bITHackaton2018
+ * Author: tsteidle
+ * Created: 09.06.18
+ */
+public class NavigationService {
+}
---
+++ src/test/java/de/bithackaton/image/ImageTest.java
@@ -0,0 +1,9 @@
+package de.bithackaton.image;
+
+/**
+ * bITHackaton2018
+ * Author: tsteidle
+ * Created: 09.06.18
+ */
+public class ImageTest {
+}
